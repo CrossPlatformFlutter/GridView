9f66967de9d73334656b758e481e6ff36eb48302
TalelMejri <talelmejri8@gmail.com>
2023-08-12 18:21:52 +0100

    initProject

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
 import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(TestGridView());
+}
+
+class TestGridView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AppGrid(),
+    );
+  }
+}
+
+class AppGrid extends StatefulWidget{
+  AppGrid({super.key});
+  
+  @override
+  State<AppGrid> createState()=> _AppGrid();
+}
+
+class _AppGrid extends State<AppGrid>{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+       appBar: AppBar(title: Text("ddd")),
+    );
+  }
+}
