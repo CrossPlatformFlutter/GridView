0b55bd24d8d4560b6c447026ebcfd18db74c6826
TalelMejri <talelmejri8@gmail.com>
2023-08-12 18:50:03 +0100

    add dialog

---
+++ lib/list_cars.dart
@@ -0,0 +1,18 @@
+ import 'car.dart';
+ import 'package:flutter/material.dart';
+ 
+ List<Car> cars=[
+  Car("Audi","1",Image.asset("images/audi.jpg")),
+  Car("BMW","2",Image.asset("images/bmw.jpg")),
+  Car("dmax","3",Image.asset("images/dmax.jpg")),
+  Car("ferrari","4",Image.asset("images/ferrari.jpg")),
+  Car("Lamborghini","5",Image.asset("images/lamborghini.jpg")),
+  Car("Mercedes","6",Image.asset("images/mercedes.jpg")),
+  Car("Opel Astra","7",Image.asset("images/opelAstra.jpg")),
+  Car("Tesla","8",Image.asset("images/tesla.jpg")),
+  Car("Toyouta","9",Image.asset("images/toyouta.jpg")),
+    Car("Mercedes","10",Image.asset("images/mercedes.jpg")),
+  Car("Opel Astra","11",Image.asset("images/opelAstra.jpg")),
+  Car("Tesla","12",Image.asset("images/tesla.jpg")),
+  Car("Toyouta","13",Image.asset("images/toyouta.jpg")),
+ ];
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:gridview/car.dart';
+import 'list_cars.dart';
 
 void main(){
-  runApp(TestGridView());
+  runApp(const TestGridView());
 }
 
 class TestGridView extends StatelessWidget{
+  const TestGridView({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,17 +17,49 @@ class TestGridView extends StatelessWidget{
 }
 
 class AppGrid extends StatefulWidget{
-  AppGrid({super.key});
-  
+  const AppGrid({super.key});
+
   @override
   State<AppGrid> createState()=> _AppGrid();
 }
 
 class _AppGrid extends State<AppGrid>{
+
+final _cars = cars;
+
+           Future<void> deletecars(int index)async{
+                         await showDialog(barrierDismissible: false,context: context, builder: (context){
+                            return  SimpleDialog(
+                                title: Text("Do You Want Delete ${_cars[index].brand}"),
+                                
+                            );
+                        });
+                    }
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-       appBar: AppBar(title: Text("ddd")),
+       appBar: AppBar(title: const Text("GridView")),
+       body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
+          itemCount: cars.length,
+          itemBuilder: (context,index){
+            Car car=_cars[index];
+            return Card(
+              color:Colors.cyan,
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(15, 10, 12,12),
+                child: Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                   Expanded(child: car.image),
+                   const Divider(color: Colors.white),
+                   Text(car.model,style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 10,color: Colors.white),textAlign: TextAlign.center),
+                   Text(car.brand,style: const TextStyle(color: Colors.white),),
+                   ElevatedButton(onPressed:(){deletecars(index);} ,style: ElevatedButton.styleFrom(backgroundColor: Colors.white), child:const Icon(Icons.delete,color: Colors.red,)),
+                ]),
+              ),
+            );
+          }),
     );
   }
 }
